f1b61398e5d60184208a6c62d1f715ccb5f870c6
Edgars Vanags <e.vanags@accenture.com>
2019-05-20 18:27:10 +0300

    Added empty project

---
+++ CatJunkie-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatJunkie-iOS
+//
+//  Created by e.vanags on 20/05/2019.
+//  Copyright © 2019 esesmuedgars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
